380c7fea7b48248b88855feb253de0564e54208f
wuxingogo <52111314ly@gmail.com>
2017-04-09 18:34:58 +0800

    adjust action folder

---
+++ BehaviourTree/Scripts/ActionTemplate/BTFsm/RandomAction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using wuxingogo.btFsm;
+
+[ActionTitle("BTFsm/Random Action")]
+public class RandomAction : BTAction {
+
+    private int allEventCount = 0;
+    public override void OnAwake()
+    {
+        base.OnAwake();
+        allEventCount = Owner.totalEvent.Count;
+    }
+    public override void OnEnter()
+    {
+        base.OnEnter();
+
+        int index = Random.Range(0, allEventCount);
+        var randEvent = Owner.totalEvent[index];
+        Fsm.FireEvent(randEvent);
+    }
+}
